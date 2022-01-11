e61e93a6f6c1a412f009ad0967762ac7a188993e
Thomas Antony <mail@thomasantony.com>
2022-01-11 00:15:23 -0600

    Add default impls for pre_step and consume_buffered_key

--- src/vessel.rs
+++ src/vessel.rs
@@ -3,12 +3,12 @@ use crate::ffi::{VesselContext, DWORD};
 
 pub trait OrbiterVessel {
     fn set_class_caps(&mut self, context: &VesselContext);
-    fn pre_step(&mut self, context: &VesselContext, sim_t: f64, sim_dt: f64, mjd: f64);
+    fn pre_step(&mut self, _context: &VesselContext, _sim_t: f64, _sim_dt: f64, _mjd: f64) {}
     fn consume_buffered_key(
         &mut self,
-        context: &VesselContext,
-        key: DWORD,
-        down: bool,
-        kstate: [u8; crate::consts::LKEY_COUNT],
-    ) -> i32;
+        _context: &VesselContext,
+        _key: DWORD,
+        _down: bool,
+        _kstate: [u8; crate::consts::LKEY_COUNT],
+    ) -> i32 { 0 }
 }
