a10312ac57c5a6dc2c7696d1ba37d58c41bbfc8f
Thomas Antony <mail@thomasantony.com>
2022-01-28 20:07:02 -0600

    Add MinimalPB example

---
+++ examples/minimal/MinimalPB.rs
@@ -0,0 +1,31 @@
+use orbiter_rs::{
+   debug_string, OrbiterVessel, init_vessel, SDKVessel, FileHandle
+};
+pub struct MinimalPB
+{
+    ctx: SDKVessel
+}
+impl MinimalPB {
+    pub fn new(vessel: SDKVessel) -> Self {
+        Self {
+            ctx: vessel
+        }
+    }
+}
+impl OrbiterVessel for MinimalPB {
+    fn set_class_caps(&mut self, _cfg: FileHandle) {
+        self.ctx.SetSize(1.0);
+        self.ctx.AddMesh("ShuttlePB".into());
+    }
+    fn on_pre_step(&mut self, sim_t: f64, _sim_dt: f64, _mjd: f64)
+    {
+        debug_string!("Hello world! sim_t: {:.2}", sim_t);
+    }
+}
+
+init_vessel!(
+    fn init(vessel) {
+        MinimalPB::new(vessel)
+    }
+    fn exit() {}
+);
